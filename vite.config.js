105013f69c6c1e911753e2cd9a934f8909035632
Kazi Rayhan Reza Tirtho <thisiskazi@gmail.com>
2025-12-10 01:11:47 +0600

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
   },
   server: {
     port: 3000,
